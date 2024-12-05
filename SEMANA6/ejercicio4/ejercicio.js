629be795e8045fadf059aa0af4fbd3929281ff6c
sebastian jimenez <sebasjimenez1121@gmail.com>
2024-12-04 23:48:32 -0500

    primeros 9 ejercicios de la semana 6

---
+++ SEMANA6/ejercicio4/ejercicio.js
@@ -0,0 +1,5 @@
+const nombres = ["Carlos", "Ana", "Beatriz", "David"];
+
+const nombresOrdenados = nombres.map((nombre) => nombre.toUpperCase()).sort();
+console.log(nombresOriginales);
+console.log(nombresOrdenados);
